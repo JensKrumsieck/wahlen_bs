a74c83b0f3276b022b38b3fa989a257c523c6b05
JensKrumsieck <j.krumsieck@outlook.de>
2025-09-18 09:43:19 +0200

    doch!

--- dashboard/src/routes/timeline/+page.svelte
+++ dashboard/src/routes/timeline/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl dark:prose-invert mx-auto">
     <h2 class="text-lg font-bold tracking-tight">Zeitlicher Verlauf der Wahlergebnisse</h2>
-    <p>In dieser Ansicht können die Ergebnisse der vergangenen Wahlen in Braunschweig im zeitlichen Verlauf betrachtet werden. Kommunalwahlen werden derzeit nicht berücksichtigt.</p>
+    <p>In dieser Ansicht können die Ergebnisse der vergangenen Wahlen in Braunschweig im zeitlichen Verlauf betrachtet werden.</p>
     <Timeline {elections} {regions} />
     <p>BTW: Bundestagswahl, LTW: Landtagswahl, KoW: Kommunalwahlen, EUW: Europawahl</p>
 </div>

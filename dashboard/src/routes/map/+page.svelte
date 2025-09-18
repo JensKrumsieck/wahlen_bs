4144f7c0c7a57f3816091607ab2b4a2b08258c64
JensKrumsieck <j.krumsieck@outlook.de>
2025-09-18 09:41:26 +0200

    add text

--- dashboard/src/routes/map/+page.svelte
+++ dashboard/src/routes/map/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    import Map from '../../components/map.svelte';
+    import Map from "../../components/map.svelte";
     export let data;
     const { elections, regions } = data;
 </script>
 
 <div class="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl dark:prose-invert mx-auto">
     <h2 class="text-lg font-bold tracking-tight">Wahlergebnisse nach Stadtbezirk</h2>
-    <p>Die Karte zeigt die Ergebnisse der ausgewählten Wahl in den 12 Stadtbezirken der Stadt Braunschweig.</p>
+    <p>Die Karte zeigt die Ergebnisse der ausgewählten Wahl in den 12 Stadtbezirken der Stadt Braunschweig. Die vor 2021 existierenden 19 Stadtbezirke wurden auf gemäß ihrer Zusammenlegung auf die 12 Stadtbezirke umgerechnet.</p>
+    <p>Die Karte wurde aus dem <a href="https://www.braunschweig.de/leben/stadtplanung_bauen/geoinformationen/ogd_stadtbezirke.php" target="_blank">Shape Dateien </a> der Stadt Braunschweig erstellt.</p>
     <Map {elections} {regions} />
 </div>

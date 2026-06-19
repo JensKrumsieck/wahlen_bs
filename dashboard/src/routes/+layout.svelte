722d316ea70478d5a0cea2adb7f22478983fc92b
Jens Krumsieck <j.krumsieck@outlook.de>
2026-06-19 07:59:41 +0000

    added swetrix

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+	import { dev } from "$app/environment";
+	import * as Swetrix from "swetrix";
+
+	onMount(() => {
+		Swetrix.init("kAGRjT0W9Brg", {
+			devMode: dev,
+			disabled: dev,
+			apiURL: "https://api.analytics.jenskrumsieck.de/log",
+		});
+		Swetrix.trackViews();
+	});
+
 	import "../app.css";
 
 	let showMenu = $state(false);
@@ -17,30 +30,62 @@
 
 <header class="bg-[#00a646] p-2.5">
 	<nav class="z-20">
-		<div class="container container-sm mx-auto flex justify-between items-center">
-			<a href="/" class="font-black text-xl tracking-tight">Wahlanalyse</a>
-			<button class="md:hidden p-2" aria-label="Open menu" onclick={() => (showMenu = !showMenu)}>
-				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+		<div
+			class="container container-sm mx-auto flex justify-between items-center"
+		>
+			<a href="/" class="font-black text-xl tracking-tight">Wahlanalyse</a
+			>
+			<button
+				class="md:hidden p-2"
+				aria-label="Open menu"
+				onclick={() => (showMenu = !showMenu)}
+			>
+				<svg
+					class="w-6 h-6"
+					fill="none"
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/>
 				</svg>
 			</button>
 			<div class="hidden md:flex items-center space-x-4">
 				{#each navItems as item}
-					<a href={item.href} class="font-bold hover:bg-lime-500 p-2">{item.label}</a>
+					<a href={item.href} class="font-bold hover:bg-lime-500 p-2"
+						>{item.label}</a
+					>
 				{/each}
-				<a href="https://github.com/JensKrumsieck/wahlen_bs" target="_blank" class="hover">
+				<a
+					href="https://github.com/JensKrumsieck/wahlen_bs"
+					target="_blank"
+					class="hover"
+				>
 					<img src="github.svg" alt="GitHub" class="w-5" />
 				</a>
 			</div>
 		</div>
 		{#if showMenu}
-			<div class="md:hidden bg-[#00a646] px-4 py-2 flex flex-col space-y-2">
+			<div
+				class="md:hidden bg-[#00a646] px-4 py-2 flex flex-col space-y-2"
+			>
 				{#each navItems as item}
-					<a href={item.href} class="hover flex items-center font-bold hover:bg-lime-500 p-2">
+					<a
+						href={item.href}
+						class="hover flex items-center font-bold hover:bg-lime-500 p-2"
+					>
 						{item.label}
 					</a>
 				{/each}
-				<a href="https://github.com/JensKrumsieck/wahlen_bs" target="_blank" class="hover flex items-center">
+				<a
+					href="https://github.com/JensKrumsieck/wahlen_bs"
+					target="_blank"
+					class="hover flex items-center"
+				>
 					<img src="github.svg" alt="GitHub" class="w-5 mr-2" /> GitHub
 				</a>
 			</div>
@@ -51,14 +96,33 @@
 	<article class="pt-5">
 		{@render children()}
 	</article>
-	<footer class="p-5 mt-12 text-center container container-md mx-auto dark:text-gray-400 text-gray-600 text-sm border-t dark:border-gray-600 border-gray-50">
+	<footer
+		class="p-5 mt-12 text-center container container-md mx-auto dark:text-gray-400 text-gray-600 text-sm border-t dark:border-gray-600 border-gray-50"
+	>
 		<div class="copy">
-			Wahldaten wurden aus der <a class="underline" href="https://votemanager.kdo.de/03101000/index.html">Votemanager-Instanz der Stadt Braunschweig</a>, sowie dem
-			<a class="underline" href="https://www3.braunschweig.de/statistik/2025_Wahl-Atlas/atlas.html">Wahlatlas</a> geharvestet. Ergebnisse, die auf den alten 19 Stadtbezirken basieren, wurden entsprechend der Zusammenlegung zu den 12
-			Stadtbezirken umgerechnet. Die Daten der bundesweiten Ergebnisse wurden aus dem Portal der <a class="underline" href="https://www.bundeswahlleiterin.de/">Bundeswahlleiterin</a> entnommen.
+			Wahldaten wurden aus der <a
+				class="underline"
+				href="https://votemanager.kdo.de/03101000/index.html"
+				>Votemanager-Instanz der Stadt Braunschweig</a
+			>, sowie dem
+			<a
+				class="underline"
+				href="https://www3.braunschweig.de/statistik/2025_Wahl-Atlas/atlas.html"
+				>Wahlatlas</a
+			>
+			geharvestet. Ergebnisse, die auf den alten 19 Stadtbezirken
+			basieren, wurden entsprechend der Zusammenlegung zu den 12
+			Stadtbezirken umgerechnet. Die Daten der bundesweiten Ergebnisse
+			wurden aus dem Portal der
+			<a class="underline" href="https://www.bundeswahlleiterin.de/"
+				>Bundeswahlleiterin</a
+			> entnommen.
 		</div>
 		<div class="mt-5 text-green-200">
-			Ein Projekt von <a class="underline font-medium" href="https://jenskrumsieck.de">Dr. Jens Krumsieck</a>.
+			Ein Projekt von <a
+				class="underline font-medium"
+				href="https://jenskrumsieck.de">Dr. Jens Krumsieck</a
+			>.
 		</div>
 	</footer>
 </div>
